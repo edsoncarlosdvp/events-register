d9e7c97a15d72b865aa9d626d6465c438fcb819e
Edson Carlos <edsoncarlos@ymail.com>
2025-08-28 21:04:23 -0300

    CHORE: Project configuration

---
+++ src/test/java/br/com/edti/events_microservice/EventsMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.edti.events_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
